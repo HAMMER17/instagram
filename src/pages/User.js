a9ac93a2f845d85d519c48c78aaf94d95e74d389
HAMMER17 <plutalov70@gmail.com>
2023-05-21 22:36:07 +0300

    foto

--- src/pages/User.js
+++ src/pages/User.js
@@ -11,6 +11,7 @@ const User = () => {
   const navigate = useNavigate()
   const state = useSelector(state => state.user.value)
   const [item, setItem] = useState([])
+  const [dataImg, setDataImg] = useState([])
 
   const db = getFirestore(app)
   const getUser = async () => {
@@ -23,12 +24,22 @@ const User = () => {
     });
     setItem(arr)
   }
+  const getDataImg = async () => {
+    const querySnapshot = await getDocs(collection(db, "data"));
+    const arr = []
+    querySnapshot.forEach((doc) => {
+
+      arr.push(doc.data());
+    });
+    setDataImg(arr.concat(arr))
+  }
   useEffect(() => {
     getUser()
+    getDataImg()
     // eslint-disable-next-line 
   }, [state])
   return (
-    <div >
+    < >
       {item.map(elem => (
         <div key={elem.uid} className="user">
           <h4>{elem.email}</h4>
@@ -41,25 +52,16 @@ const User = () => {
           {/* <input type="text" placeholder="SMS..." /> */}
           <p>9 888 444 333 111</p>
           <div className="user_photo">
-            <img src="/images/camera.webp" alt="photo1" style={{ width: 80, height: 80 }} />
-            <img src="/images/t.jpg" alt="photo1" style={{ width: 80, height: 80 }} />
-            <img src="/images/data.webp" alt="photo1" style={{ width: 80, height: 80 }} />
-            <img src="/images/manki.webp" alt="photo1" style={{ width: 80, height: 80 }} />
-            <img src="/images/tiger.webp" alt="photo1" style={{ width: 80, height: 80 }} />
-            <img src="/images/tree1.webp" alt="photo1" style={{ width: 80, height: 80 }} />
-            <img src="/images/camera.webp" alt="photo1" style={{ width: 80, height: 80 }} />
-            <img src="/images/t.jpg" alt="photo1" style={{ width: 80, height: 80 }} />
-            <img src="/images/data.webp" alt="photo1" style={{ width: 80, height: 80 }} />
-            <img src="/images/manki.webp" alt="photo1" style={{ width: 80, height: 80 }} />
-            <img src="/images/tiger.webp" alt="photo1" style={{ width: 80, height: 80 }} />
-            <img src="/images/tree1.webp" alt="photo1" style={{ width: 80, height: 80 }} />
+            {dataImg.map(elem => (
+              <img src={elem.img} alt="photo1" style={{ width: 80, height: 80 }} key={Math.random()} />
+            ))}
           </div>
 
         </div>
       ))}
 
       <Footer />
-    </div>
+    </>
   )
 }
 
